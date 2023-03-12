673b6a13c6ac4d61dddbe080434b8dd169b6c7fd
KenStarry <starrycodes@gmail.com>
2023-03-13 01:55:46 +0300

    Added Image

--- DesktopImages/src/jvmMain/kotlin/Main.kt
+++ DesktopImages/src/jvmMain/kotlin/Main.kt
@@ -1,5 +1,8 @@
 import androidx.compose.material.MaterialTheme
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -7,8 +10,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 
 @Composable
 @Preview
@@ -16,16 +24,24 @@ fun App() {
     var text by remember { mutableStateOf("Hello, World!") }
 
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
-        }
+        //  image
+        Image(
+            painter = painterResource("house.jpg"),
+            contentDescription = "Sample",
+            modifier = Modifier
+                .size(300.dp)
+        )
     }
 }
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
+    Window(
+        onCloseRequest = ::exitApplication,
+        //  window title
+        title = "Images",
+        state = rememberWindowState(width = 800.dp, height = 700.dp)
+        //  window icon
+    ) {
         App()
     }
 }
