54277a7180f10b6a0a3a982edd2d13e6853c0d16
KenStarry <starrycodes@gmail.com>
2023-03-13 20:44:30 +0300

    Main content

--- MedPlus/src/jvmMain/kotlin/Main.kt
+++ MedPlus/src/jvmMain/kotlin/Main.kt
@@ -1,5 +1,9 @@
 import androidx.compose.material.MaterialTheme
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -7,19 +11,32 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
 @Composable
 @Preview
 fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
-
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+
+        //  Main UI
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White),
+            contentAlignment = Alignment.Center
+        ) {
+            MainContent(
+                modifier = Modifier
+                    .clip(RoundedCornerShape(24.dp))
+                    .fillMaxSize(0.8f)
+                    .background(Color(0xFFEFEFEF))
+            )
         }
     }
 }
---
+++ MedPlus/src/jvmMain/kotlin/MainContent.kt
@@ -0,0 +1,16 @@
+import androidx.compose.foundation.layout.Row
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MainContent(
+    modifier: Modifier = Modifier
+) {
+
+    Row(
+        modifier = modifier
+    ) {
+
+    }
+
+}
